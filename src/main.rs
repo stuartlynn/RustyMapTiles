176d32ac7955d70475acd1c9ecc2645574c52a97
Stuart Lynn <stuart.lynn@gmail.com>
2019-11-15 09:41:04 -0500

    Generalizing to an arbitrary table and stripping out OD stuff

--- src/main.rs
+++ src/main.rs
@@ -31,14 +31,6 @@ struct Tile {
   mvt: Vec<u8>,
 }
 
-#[derive(QueryableByName, PartialEq, Debug, Serialize, Deserialize)]
-struct OD {
-  #[sql_type = "Text"]
-  id: String,
-  #[sql_type = "Integer"]
-  count: i32,
-}
-
 fn bbox(x: u32, y: u32, z: u32) -> [f64; 4] {
   let max = 6378137.0 * PI;
   let res = max * 2.0 / (2.0 as f64).powf(z as f64) as f64;
@@ -51,72 +43,9 @@ fn bbox(x: u32, y: u32, z: u32) -> [f64; 4] {
   ]
 }
 
-#[database("census_geoms")]
-struct CensusDbConn(diesel::pg::PgConnection);
+#[database("database")]
+struct DbConn(diesel::pg::PgConnection);
 
-#[get("/hello/<name>/<age>")]
-fn hello(name: String, age: u8) -> String {
-  format!("Hello, {} year old named {}!", age, name)
-}
-
-#[get("/od_multi/<origin>/<ids>")]
-fn od_multi(conn: CensusDbConn, origin: String, ids: String) -> JsonValue {
-  let origin_column = match origin.as_ref() {
-    "home" => "h_geocode",
-    "work" => "w_geocode",
-    _ => panic!("needs to be work or home"),
-  };
-
-  let dest_column = match origin.as_ref() {
-    "home" => "w_geocode",
-    "work" => "h_geocode",
-    _ => panic!("needs to be work or home"),
-  };
-
-  let query = format!(
-    "select {dest_column}::TEXT as id, SUM(\"S000\")::INTEGER as count  from lodes_od where
-    {origin_column} IN ({ids}) 
-    group by {dest_column} 
-    ",
-    origin_column = origin_column,
-    dest_column = dest_column,
-    ids = ids
-  );
-
-  let result: Vec<OD> = sql_query(query)
-    .get_results(&*conn)
-    .expect("query failed to run");
-  json!(result)
-}
-
-#[get("/od/<origin>/<id>")]
-fn od(conn: CensusDbConn, origin: String, id: String) -> JsonValue {
-  let origin_column = match origin.as_ref() {
-    "home" => "h_geocode",
-    "work" => "w_geocode",
-    _ => panic!("needs to be work or home"),
-  };
-
-  let dest_column = match origin.as_ref() {
-    "home" => "w_geocode",
-    "work" => "h_geocode",
-    _ => panic!("needs to be work or home"),
-  };
-
-  let query = format!(
-    "select {dest_column}::TEXT as id, \"S000\"::INTEGER as count  from lodes_od where
-    {origin_column} = \'{id}\' 
-    ",
-    origin_column = origin_column,
-    dest_column = dest_column,
-    id = id
-  );
-
-  let result: Vec<OD> = sql_query(query)
-    .get_results(&*conn)
-    .expect("query failed to run");
-  json!(result)
-}
 
 #[get("/<tile_type>/<z>/<x>/<y>")]
 fn tile(conn: CensusDbConn, tile_type: String, z: u32, x: u32, y: u32) -> Vec<u8> {
@@ -132,6 +61,7 @@ fn tile(conn: CensusDbConn, tile_type: String, z: u32, x: u32, y: u32) -> Vec<u8
     SELECT ST_AsMVT(q, '{tile_table}', 4096, 'mvt_geom') as mvt 
     FROM (
       SELECT
+          *,
           \"GEOID\" as id,
           ST_AsMVTGeom(
               geom,
@@ -141,7 +71,7 @@ fn tile(conn: CensusDbConn, tile_type: String, z: u32, x: u32, y: u32) -> Vec<u8
               true
           ) mvt_geom
       FROM (
-        select \"GEOID\", geometry as geom from {tile_table}
+        select *, geometry as geom from {tile_table}
         where geometry && ST_TRANSFORM(ST_MakeEnvelope({x_min}, {y_min}, {x_max}, {y_max}, 3857), 4326)
       ) c
     ) q
@@ -154,7 +84,6 @@ fn tile(conn: CensusDbConn, tile_type: String, z: u32, x: u32, y: u32) -> Vec<u8
     x_max = tile_box[2],
     y_max = tile_box[3]
   );
-  println!("{}", query);
 
   let result: Tile = sql_query(query)
     .get_result(&*conn)
@@ -165,8 +94,8 @@ fn tile(conn: CensusDbConn, tile_type: String, z: u32, x: u32, y: u32) -> Vec<u8
 
 fn main() {
   rocket::ignite()
-    .attach(CensusDbConn::fairing())
-    .mount("/", routes![hello, tile, od, od_multi])
+    .attach(DbConn::fairing())
+    .mount("/", routes![ tile])
     .mount("/public", StaticFiles::from("./public"))
     .launch();
 }
